091962ca4a9038a205f2642e088bca21ac672816
pezhmanparsaee <p.parsaee@gmail.com>
2020-09-26 12:42:42 +0330

    add a sample for iterating an array using Symbol.iterator

---
+++ src/iterators/arr.js
@@ -0,0 +1,16 @@
+var arr = [10, 20, 30];
+
+for (var item of arr) {
+  console.log(item);
+}
+
+var it = arr[Symbol.iterator]();
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+
+var it2 = arr[Symbol.iterator]();
+for(let x; (x = it2.next()).done === false;) {
+  console.log(x.value);
+}
